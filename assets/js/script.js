15f0d938976bf83352657fcd47d4b03ce2b6867a
iamanandhuam <anandhuanandanan@gamil.com>
2023-11-18 12:41:38 +0530

    Footer icon updation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 // Start the slideshow when the page loads
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("TODO")
+document.addEventListener('DOMContentLoaded', function () {
+    var icons = document.querySelectorAll('.footer-icons a');
+
+    icons.forEach(function (icon) {
+        var originalSrc = icon.querySelector('img').src;
+        var hoverSrc = icon.dataset.hoverSrc;
+
+        icon.addEventListener('mouseenter', function () {
+            icon.querySelector('img').src = hoverSrc;
+        });
+
+        icon.addEventListener('mouseleave', function () {
+            icon.querySelector('img').src = originalSrc;
+        });
+    });
 });
